4e55178fb17efa06bd356d7ec27b4b73dfc34587
Eliot <43924629+Eliot-M@users.noreply.github.com>
2018-10-12 15:58:40 +0200

    Update script.py

--- script.py
+++ script.py
@@ -195,7 +195,8 @@ def computeColorblind(df_colors, colors_column, transform_type):
     
     return df
 
-        
+##### Analysis functions #####
+
 # --- Compute average color comprehensibility --- #        
         
 def colorComprehensibility(df_colors, colors_column, analysis_type):
@@ -229,7 +230,171 @@ def colorComprehensibility(df_colors, colors_column, analysis_type):
         print("KeyError: invalid column name")
         
 
+# --- Compute Global deviation --- #
+ 
+def colorDeviation(df_colors, colors_column, analysis_type = 'all'):
+    '''Goal: Compute de deviation between normal vision and a colorblind vision according to a criteria
+    
+    Set analysis_type to 'average' to compute the average distance between colors.
+        To 'averagePond' to compute the average distance between colors including with size ponderation
+        To 'minDiff' to find the maximum difference between two colors.
+        To 'all' (default) to perform the 3 previous analysis
+        
+    Input: Pandas dataframe - Most common colors
+    Output: list of color comprehension computation
+    '''
+
+    try: 
+        #Create color datasets
+        pro = computeColorblind(df_colors, colors_column, 1)
+        deu = computeColorblind(df_colors, colors_column, 2)
+        tri = computeColorblind(df_colors, colors_column, 3)
+        
+        #prevent crash if the analysis_type is wrong
+        ['average','averagePond','minDiff','all'].index(analysis_type)
 
         
+        if analysis_type == 'all':
+            avg = colorComprehensibility(df_colors, colors_column, analysis_type = 'average')
+            avgpond = colorComprehensibility(df_colors, colors_column, analysis_type = 'averagePond')
+            mind = colorComprehensibility(df_colors, colors_column, analysis_type = 'minDiff')
         
+            #Pro color
+            avgP = colorComprehensibility(pro, colors_column, analysis_type = 'average')
+            avgpondP = colorComprehensibility(pro, colors_column, analysis_type = 'averagePond')
+            mindP = colorComprehensibility(pro, colors_column, analysis_type = 'minDiff')
+            
+            #Deu color
+            avgD = colorComprehensibility(deu, colors_column, analysis_type = 'average')
+            avgpondD = colorComprehensibility(deu, colors_column, analysis_type = 'averagePond')
+            mindD = colorComprehensibility(deu, colors_column, analysis_type = 'minDiff')
+            
+            #Tri color
+            avgT = colorComprehensibility(tri, colors_column, analysis_type = 'average')
+            avgpondT = colorComprehensibility(tri, colors_column, analysis_type = 'averagePond')
+            mindT = colorComprehensibility(tri, colors_column, analysis_type = 'minDiff')
+            
+            return [avg, avgP, avgD, avgT, avgpond, avgpondP, avgpondD, avgpondT, mind, mindP, mindD, mindT]
         
+        if analysis_type == 'average':
+            avg = colorComprehensibility(df_colors, colors_column, analysis_type = 'average')
+            avgP = colorComprehensibility(pro, colors_column, analysis_type = 'average')
+            avgD = colorComprehensibility(deu, colors_column, analysis_type = 'average')
+            avgT = colorComprehensibility(tri, colors_column, analysis_type = 'average')
+            
+            return [avg, avgP, avgD, avgT]
+        
+            
+        if analysis_type == 'averagePond':
+            avgpond = colorComprehensibility(df_colors, colors_column, analysis_type = 'averagePond')
+            avgpondP = colorComprehensibility(pro, colors_column, analysis_type = 'averagePond')
+            avgpondD = colorComprehensibility(deu, colors_column, analysis_type = 'averagePond')
+            avgpondT = colorComprehensibility(tri, colors_column, analysis_type = 'averagePond')
+            
+            return [avgpond, avgpondP, avgpondD, avgpondT]
+        
+        if analysis_type == 'minDiff':
+            mind = colorComprehensibility(df_colors, colors_column, analysis_type = 'minDiff')
+            mindP = colorComprehensibility(pro, colors_column, analysis_type = 'minDiff')
+            mindD = colorComprehensibility(deu, colors_column, analysis_type = 'minDiff')
+            mindT = colorComprehensibility(tri, colors_column, analysis_type = 'minDiff')
+        
+            return [mind, mindP, mindD, mindT]
+        
+        if min(RGBcolor) < 0:
+            raise ValueError()
+    
+    except TypeError:
+        print("TypeError: df_colors must be a pandas dataframe, generated from the getMostCommonColor() function.")
+        
+    except ValueError:
+        print("ValueError: Use one of the following possibility as a analysis_type :'all', 'average', 'averagePond','minDiff'.")
+           
+        
+# --- Ratio sub-function --- #
+
+def factorFromVect(vector):
+    '''Goal: From a vector of length 4, divided the first element by the others
+    Input: list of length 4
+    Output: list of length 3 - ratio of comprehensibility'''
+    try:
+        if len(vector) != 4:
+            raise IndexError()
+        return [vector[0]/vector[1], vector[0]/vector[2], vector[0]/vector[3]]
+
+    except IndexError: 
+        print("List must be of length 4")
+    except ZeroDivisionError: 
+        print("Issues in the color perception value : zero raised")
+ 
+
+# --- Raise warning for colorblind issues --- #
+
+def colorAnalysis(df_colors, colors_column, analysis_type = 'all', threshold = 5):
+    '''Create all possibilities and analyse colors differences
+    
+    Set analysis_type to 'average' to compute the average distance between colors.
+        To 'averagePond' to compute the average distance between colors including with size ponderation
+        To 'minDiff' to find the maximum difference between two colors.
+        To 'all' (default) to perform the 3 previous analysis
+    Set threshold to the minimum ratio you wish not to surpass. 
+        For example : 5 to raise a warning if it's 5 times more difficult for a colorblind person to distinguish a color.
+    
+    Input: Pandas dataframe - Most common colors
+    Output: Print of result'''
+    isOk = True
+    
+    try:
+        #prevent crash if the analysis_type is wrong
+        ['average','averagePond','minDiff','all'].index(analysis_type)
+        if int(threshold) < 1:
+            raise ValueError()
+        
+        list_perception = colorDeviation(df_colors, colors_column, analysis_type)
+    
+        if analysis_type == 'all':
+            out = []
+            for x in [0,1,2]:
+                sub_list_perception = list_perception[x*4:(x+1)*4]
+                res = factorFromVect(sub_list_perception)
+                measure = ['average distance comprehensibility between colors. It\'s ', 'average distance with ponderation on the area covered comprehensibility between colors. It\'s ','minimum difference comprehensibility between colors. It\'s '][x]
+                vision_issues = list([['Issues for protanopia according to ', 'Issues for deuteranopia according to ','Issues for tritanopia according to'][index] for index in [x[0] for x in list(enumerate(res)) if x[1]>threshold]])
+                values_issues = list([value for value in res if value > threshold])
+                out.append([str(vision_issues[x] + measure + str(round(values_issues[x],1)) + ' times more difficult for them to make the difference.') for x in list(range(len(vision_issues)))])
+                
+            if len(out) >= 1:
+                isOk = False
+                outList = [item for sublist in out for item in sublist]
+                print('-------\n')
+                print('\n'.join(outList))
+        
+        else:
+            if analysis_type == 'average':
+                measure = 'average distance comprehensibility between colors. It\'s '
+            if analysis_type == 'averagePond':
+                measure = 'average distance with ponderation on the area covered comprehensibility between colors. It\'s '
+            if analysis_type == 'minDiff':
+                measure = 'minimum difference comprehensibility between colors. It\'s '
+
+            res = factorFromVect(list_perception)
+
+            vision_issues = list([['Issues for protanopia according to ', 'Issues for deuteranopia according to ','Issues for tritanopia according to'][index] for index in [x[0] for x in list(enumerate(res)) if x[1]>threshold]])
+            values_issues = list([value for value in res if value > threshold])
+            
+            if len(vision_issues) >= 1:
+                isOk = False
+                print('-------\n')
+                print('\n'.join([str(vision_issues[x] + measure + str(round(values_issues[x],1)) + ' times more difficult for them to make the difference.') for x in list(range(len(vision_issues)))]))
+        if isOk == True:
+            print('-------\n')
+            print('There is no warning due to colorblind about colors used in the image')
+            
+    except ValueError:
+        print("ValueError: Use one of the following possibility as a analysis_type :'all', 'average', 'averagePond','minDiff'. The threshold must be numeric and positive")
+
+        
+##### Run an example #####
+
+df_colors = getMostCommonColor(getImageRGB("/Users/eliotm/Documents/duck.png"),top_colors = 10, grey_scale = False, approx_grey = True)
+colorAnalysis(df_colors, 'color', analysis_type = 'all', threshold = 2)   
+
